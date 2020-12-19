4b24e1cee72d81f8e3c32cdfea9e7ea9d042c275
Oleksii Kostetskyi <oleksii.mld@gmail.com>
2020-12-19 13:32:34 +0200

    task06: move Web Services publishing to configuration

--- src/main/java/ua/omld/jpc/AppLauncher.java
+++ src/main/java/ua/omld/jpc/AppLauncher.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.GenericApplicationContext;
 import ua.omld.jpc.configuration.BasicConfiguration;
 import ua.omld.jpc.configuration.WebStartCondition;
-import ua.omld.jpc.ws.ServicePublisher;
 
 /**
  * @author Oleksii Kostetskyi
@@ -14,11 +13,7 @@ public class AppLauncher {
 	public void start() {
 		GenericApplicationContext context = new AnnotationConfigApplicationContext();
 		context.getEnvironment().setActiveProfiles(WebStartCondition.LOCAL);
-		context.registerBean(BasicConfiguration.class );
+		context.registerBean(BasicConfiguration.class);
 		context.refresh();
-		ServicePublisher publisher = context.getBean(ServicePublisher.class);
-		publisher.publishReportService();
-		publisher.publishActivityService();
-		publisher.publishBuildingService();
 	}
 }
--- src/main/java/ua/omld/jpc/configuration/WSConfig.java
+++ src/main/java/ua/omld/jpc/configuration/WSConfig.java
@@ -1,9 +1,11 @@
 package ua.omld.jpc.configuration;
 
 import jpc.omld.ua.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ua.omld.jpc.ws.ServicePublisher;
 
 /**
  * @author Oleksii Kostetskyi
@@ -22,5 +24,12 @@ public class WSConfig {
 	@Bean
 	public ObjectFactory ObjectFactory() {
 		return new ObjectFactory();
+	}
+
+	@Autowired
+	public void publishJaxWsServices(ServicePublisher servicePublisher) {
+		servicePublisher.publishActivityService();
+		servicePublisher.publishBuildingService();
+		servicePublisher.publishReportService();
 	}
 }
