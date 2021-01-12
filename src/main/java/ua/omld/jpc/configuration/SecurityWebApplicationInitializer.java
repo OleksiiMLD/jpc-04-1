a87b61dd96c397ec77530f7327c9923c7325c229
Oleksii Kostetskyi <oleksii.mld@gmail.com>
2021-01-12 18:09:19 +0200

    add simple security configuration

---
+++ src/main/java/ua/omld/jpc/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package ua.omld.jpc.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Oleksii Kostetskyi
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ src/main/java/ua/omld/jpc/configuration/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package ua.omld.jpc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Oleksii Kostetskyi
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers("/ws/**").permitAll()
+				.antMatchers("/api/**").authenticated()
+				.antMatchers("/**").denyAll()
+			.and()
+				.httpBasic()
+				.realmName("JPC")
+			.and()
+				.csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("{noop}user").roles("USER")
+			.and()
+				.withUser("manager").password("{noop}manager").roles("USER", "MANAGER")
+			.and()
+				.withUser("admin").password("{noop}admin").roles("USER", "MANAGER", "ADMIN");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+}
